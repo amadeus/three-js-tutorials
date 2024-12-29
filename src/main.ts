da2ed0b18ba0cbf9053e55be8ab05ec18305dd9f
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2024-12-29 16:03:27 +0100

    Manual: Three Rotating Block Intro

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,58 @@
 import "./style.css";
 
-import * as THREE from "three";
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshPhongMaterial,
+  Mesh,
+  DirectionalLight,
+  type BufferGeometry,
+} from "three";
 
 const dpi = window.devicePixelRatio ?? 1;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
+const scene = new Scene();
+const camera = new PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000,
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth * dpi, window.innerHeight * dpi, false);
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+function makeInstance(geometry: BufferGeometry, color: number, x: number) {
+  const material = new MeshPhongMaterial({ color });
+  const cube = new Mesh(geometry, material);
+  cube.position.x = x;
+  scene.add(cube);
+  return cube;
+}
+
+const geometry = new BoxGeometry(1, 1, 1);
+
+const cubes = [
+  makeInstance(geometry, 0x44aa88, 0),
+  makeInstance(geometry, 0x8844aa, -2),
+  makeInstance(geometry, 0xaa8844, 2),
+];
+
 camera.position.z = 5;
 
+const light = new DirectionalLight(0xffffff, 3);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  for (const cube of cubes) {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+  }
   renderer.render(scene, camera);
 }
 
