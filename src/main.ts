6526b630feb717360b7e147b9987167af042e1de
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2024-12-29 14:54:42 +0100

    Tutorial Complete: Line Drawing

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,43 @@
 import "./style.css";
 
-import * as THREE from "three";
+import {
+  WebGLRenderer,
+  PerspectiveCamera,
+  Scene,
+  LineBasicMaterial,
+  Vector3,
+  BufferGeometry,
+  Line,
+} from "three";
 
 const dpi = window.devicePixelRatio ?? 1;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
-);
-
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth * dpi, window.innerHeight * dpi, false);
 
+const camera = new PerspectiveCamera(
+  45,
+  (window.innerWidth * dpi) / (window.innerHeight * dpi),
+  1,
+  500,
+);
+
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+const scene = new Scene();
+
+const material = new LineBasicMaterial({ color: 0x0000ff });
+
+const points = [
+  new Vector3(-10, 0, 0),
+  new Vector3(0, 10, 0),
+  new Vector3(10, 0, 0),
+];
+
+const geometry = new BufferGeometry().setFromPoints(points);
+const line = new Line(geometry, material);
+scene.add(line);
+renderer.render(scene, camera);
+
 document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 5;
-
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-}
-
-renderer.setAnimationLoop(animate);
