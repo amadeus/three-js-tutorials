205a1aff1f9e3a63f985a2af2f76f985db1881b0
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-01-04 15:29:55 -0800

    Manual: Texture Wrapping

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,52 @@ import {
   MeshBasicMaterial,
   Mesh,
   DirectionalLight,
+  MathUtils,
   TextureLoader,
   SRGBColorSpace,
   LoadingManager,
+  ClampToEdgeWrapping,
+  RepeatWrapping,
+  MirroredRepeatWrapping,
   type BufferGeometry,
 } from 'three';
+import {GUI} from 'three/addons/libs/lil-gui.module.min.js';
+
+class BaseHelper {
+  obj: Record<string, any>;
+  prop: string;
+
+  constructor(obj: Record<string, any>, prop: string) {
+    this.obj = obj;
+    this.prop = prop;
+  }
+}
+
+class DegRadHelper extends BaseHelper {
+  get value(): number {
+    return MathUtils.radToDeg(this.obj[this.prop]);
+  }
+
+  set value(v: number) {
+    this.obj[this.prop] = MathUtils.degToRad(v);
+  }
+}
+
+class StringToNumberHelper extends BaseHelper {
+  get value(): number {
+    return this.obj[this.prop];
+  }
+
+  set value(v: string) {
+    this.obj[this.prop] = parseFloat(v);
+  }
+}
+
+const wrapModes = {
+  ClampToEdgeWrapping,
+  RepeatWrapping,
+  MirroredRepeatWrapping,
+};
 
 const dpi = window.devicePixelRatio ?? 1;
 
@@ -40,25 +81,14 @@ renderer.setSize(window.innerWidth * dpi, window.innerHeight * dpi, false);
 
 document.body.appendChild(renderer.domElement);
 
-function loadColorTexture(path: string) {
-  const texture = loader.load(path);
-  texture.colorSpace = SRGBColorSpace;
-  return texture;
-}
-
 const loadManager = new LoadingManager();
 const loader = new TextureLoader(loadManager);
-const materials = [
-  new MeshBasicMaterial({map: loadColorTexture('/flower-1.jpg')}),
-  new MeshBasicMaterial({map: loadColorTexture('/flower-2.jpg')}),
-  new MeshBasicMaterial({map: loadColorTexture('/flower-3.jpg')}),
-  new MeshBasicMaterial({map: loadColorTexture('/flower-4.jpg')}),
-  new MeshBasicMaterial({map: loadColorTexture('/flower-5.jpg')}),
-  new MeshBasicMaterial({map: loadColorTexture('/flower-6.jpg')}),
-];
+const texture = loader.load('/wall.jpg');
+texture.colorSpace = SRGBColorSpace;
+const material = new MeshBasicMaterial({map: texture});
 
 function makeInstance(geometry: BufferGeometry, x: number) {
-  const cube = new Mesh(geometry, materials);
+  const cube = new Mesh(geometry, material);
   cube.position.x = x;
   scene.add(cube);
   return cube;
@@ -78,6 +108,21 @@ const progressBarElem = loadingElem.querySelector('.progressbar');
 if (!(progressBarElem instanceof HTMLElement)) {
   throw new Error(`progressBarElem doesn't exist`);
 }
+
+function updateTexture() {
+  texture.needsUpdate = true;
+}
+
+const gui = new GUI();
+gui.add(new StringToNumberHelper(texture, 'wrapS'), 'value', wrapModes).name('texture.wrapS').onChange(updateTexture);
+gui.add(new StringToNumberHelper(texture, 'wrapT'), 'value', wrapModes).name('wrapT').onChange(updateTexture);
+gui.add(texture.repeat, 'x', 0, 5, 0.1).name('texture.repeat.x');
+gui.add(texture.repeat, 'y', 0, 5, 0.1).name('texture.repeat.y');
+gui.add(texture.offset, 'x', 0, 5, 0.1).name('texture.offset.x');
+gui.add(texture.offset, 'y', 0, 5, 0.1).name('texture.offset.y');
+gui.add(texture.center, 'x', 0, 5, 0.1).name('texture.center.x');
+gui.add(texture.center, 'y', 0, 5, 0.1).name('texture.center.y');
+gui.add(new DegRadHelper(texture, 'rotation'), 'value', -360, 360).name('texture.rotation');
 
 loadManager.onProgress = (_urlOfLastItemLoaded: string, itemsLoaded: number, itemsTotal: number) => {
   const progress = itemsLoaded / itemsTotal;
